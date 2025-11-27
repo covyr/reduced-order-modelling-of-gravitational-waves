d5c948dfe3d9a74d9c82b915ebacbea2d2a8ebdb
Connor Campbell <covyr.io@gmail.com>
2025-11-27 19:14:18 +0000

    greedy_ei.py - removed unnecessary comments, added verbose argument to main(), and removed outdated imports

--- scripts/greedy_ei.py
+++ scripts/greedy_ei.py
@@ -1,7 +1,5 @@
-from typing import Tuple
 import numpy as np
 
-# from config import COMPONENTS, DATA_DIR, MODELS_DIR, MODES
 from romgw.config.env import PROJECT_ROOT
 from romgw.maths.ei import empirical_time_nodes
 from romgw.typing.core import BBHSpinType, ModeType, ComponentType
@@ -13,6 +11,7 @@ def main(
     mode: ModeType,
     component: ComponentType,
     saving: bool = False,
+    verbose: bool = False,
 ) -> None:
     """"""
     # Validate literals. Raises exception if invalid.
@@ -20,34 +19,28 @@ def main(
     mode = validate_literal(mode, ModeType)
     component = validate_literal(component, ComponentType)
 
-    print(f"Finding empirical times ({bbh_spin=}, {mode=}, {component=}).")
+    if verbose:
+        print(f"Finding empirical times for {bbh_spin=}, {mode=}, {component=}")
     
-    # Root directory for IO operations.
     data_dir = PROJECT_ROOT / "data" / bbh_spin / "train" / mode / component
 
-    
-    # Load reduced basis elements.
     rb_dir = data_dir / "reduced_basis" / "elements"
     rb = ComponentWaveformDataset.from_directory(rb_dir, component=component)
     
-    # Find empirical time nodes and the B matrix.
     etns, B = empirical_time_nodes(rb)
-    print(f"{etns=}\n{etns.shape=}")
-    print(f"{B=}\n{B.shape=}")
+    if verbose:
+        print(f"{etns=}\n{etns.shape=}")
+        print(f"{B=}\n{B.shape=}")
 
-    # Check that the empirical time nodes are unique (they *must* be).
-    if len(etns) != len(set(etns)):
+    if len(etns) != len(set(etns)):  # empirical time nodes must be unique
         raise ValueError("Empirical time nodes must be unique.")
     
     if saving:
-        # Root directory for IO operations for empirical interpolation data.
         ei_dir = data_dir / "empirical_interpolation"
         
-        # Save empirical time nodes.
         empirical_time_node_file = ei_dir / "empirical_time_nodes.npy"
         np.save(empirical_time_node_file, etns, allow_pickle=False)
 
-        # Save B matrix.
         B_matrix_file = ei_dir / "B_matrix.npy"
         np.save(B_matrix_file, B, allow_pickle=False)
 
@@ -56,4 +49,5 @@ if __name__ == "__main__":
     main(bbh_spin="NS",
          mode="2,2",
          component="amplitude",
-         saving=True)
+         saving=True,
+         verbose=True)
